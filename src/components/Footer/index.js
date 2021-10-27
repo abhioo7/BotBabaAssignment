07a76bf24cf3388229a4dcb607bbecc63b5ff3cb
abhioo7 <guptaabhi9650@gmail.com>
2021-10-27 10:36:57 +0530

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Styles
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="copyright">App for Assignment Purpose Only</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
