fdd8f3b4cc707786bc73c763aa3f07c949946ffb
abhioo7 <guptaabhi9650@gmail.com>
2021-10-27 11:06:18 +0530

    Updates

---
+++ precache-manifest.23fdc586c8f959e8399af3f25fcbc1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a465f0657e091fced59e2e4b8451857",
+    "url": "/BotBabaAssignment/index.html"
+  },
+  {
+    "revision": "cde0d90fdd56983a76ed",
+    "url": "/BotBabaAssignment/static/css/main.383432e9.chunk.css"
+  },
+  {
+    "revision": "0451511e8e380c1a0fa0",
+    "url": "/BotBabaAssignment/static/js/2.03e82964.chunk.js"
+  },
+  {
+    "revision": "d7b6ce2de861a9d04626a0650ef3727b",
+    "url": "/BotBabaAssignment/static/js/2.03e82964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde0d90fdd56983a76ed",
+    "url": "/BotBabaAssignment/static/js/main.9fb98703.chunk.js"
+  },
+  {
+    "revision": "5390be616f846faf794e",
+    "url": "/BotBabaAssignment/static/js/runtime-main.718674a8.js"
+  },
+  {
+    "revision": "6a51178c4c213bbde5487bfb5f652177",
+    "url": "/BotBabaAssignment/static/media/placeholder-user.6a51178c.jpg"
+  }
+]);
